5c1f4aaea8ccb255e166aad899d705cbc7d6f7a8
Liub000v <62075023+Liub000v@users.noreply.github.com>
2021-12-09 20:04:53 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+import http from 'http';
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  };
+const server = http.createServer(async(req,res)=>{
+      if(req.url === '/result4/'){
+          
+          res.writeHead(200,{'Content-Type':'application/json', ...CORS})
+          let data = '';
+          await req.on('data', function(chunk){
+            data += chunk;
+            }).on('end', () => {
+          })
+          let header = req.headers['x-test'];
+          res.write(JSON.stringify({
+            "message":"id472956843",
+            "x-result": header,
+            "x-body":data
+            }
+            ));
+      }
+      res.end('\n')
+      
+  });
+  let port_number = process.env.PORT || 4321;
+  server.listen(port_number);
